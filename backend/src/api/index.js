92544f76e1c4f31340dc07e1647f099b9bbc49a1
Chirzin Attabiq <chirzin.attabiq@gmail.com>
2025-09-24 20:46:09 +0700

    update: index.js dan vercel.json

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -3,19 +3,21 @@ import userRoute from "../routes/userRoute.js";
 import errorHandler from "../middlewares/errorHandler.js";
 import { testDBConnection } from '../config/db.js';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDoc from '../docs/swagger.json' with { type: 'json' };
+import swaggerDoc from '../docs/swagger.json' assert { type: 'json' };
 import dotenv from 'dotenv';
 import cors from 'cors';
+
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: false
 }));
 
 app.use(express.json());
+
 app.use('/api/users', userRoute);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
@@ -25,7 +27,8 @@ app.use((req, res, next) => {
 
 app.use(errorHandler);
 
-app.listen(port, async () => {
-    console.log(`API running on port ${port}`);
-    await testDBConnection();
-});
+export default app;
+
+testDBConnection().catch(err => {
+    console.error("DB connection failed: ", err);
+})
