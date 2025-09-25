1ccdba3e5fec92c9657b1a1562c2f8ee4d6d6d4e
Chirzin Attabiq <chirzin.attabiq@gmail.com>
2025-09-25 15:50:42 +0700

    update: index.js

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,10 @@ app.use(express.json());
 app.use('/api/users', userRoute);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
+app.use("/", (req, res, next) => {
+    res.send("Server is running");
+});
+
 app.use((req, res, next) => {
     res.status(404).json({ message: 'Endpoint tidak ditemukan!' });
 });
